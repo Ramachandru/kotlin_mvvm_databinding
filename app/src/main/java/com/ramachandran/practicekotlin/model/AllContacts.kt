8b3a67098198a266a2804beebe9c1dc04a2879f1
rkv <ramachandru6@gmail.com>
2021-12-25 15:15:06 +0530

    removed unwanted code

--- app/src/main/java/com/ramachandran/practicekotlin/model/AllContacts.kt
+++ app/src/main/java/com/ramachandran/practicekotlin/model/AllContacts.kt
@@ -1,17 +1,10 @@
 package com.ramachandran.practicekotlin.model
 
-data class ContactDetails(val contacts : ArrayList<Contacts>){
-
-}
-
+data class ContactDetails(val contacts : ArrayList<Contacts>)
 data class Contacts(val id: String,
                     val name : String,
                     val email : String,
                     val address : String,
                     val gender : String,
-                    val phone : Phone){
-
-}
-data class Phone(val mobile : String){
-
-}
+                    val phone : Phone)
+data class Phone(val mobile : String)
